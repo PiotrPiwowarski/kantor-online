535c964dd5625477a82b52c263e31f59cf5df049
Piotr Piwowarski <piotr@piwowarski-uniet.com>
2024-10-07 08:35:33 +0200

    initial commit

---
+++ src/main/java/com/example/kantoronline/KantorOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.kantoronline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KantorOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KantorOnlineApplication.class, args);
+    }
+
+}
